2b56472df69cd14b96f841f7c62eb9980957ec3d
ttdesign <tkthedesigner@gmail.com>
2020-06-05 16:35:16 -0700

    -install allauth and migrate

--- swirl_django/swirl/models.py
+++ swirl_django/swirl/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+Espresso = (
+    ('signature','Espresso Signature Blend'),
+    ('blonde','Espresso blonde'),
+    ('darkroasted','Darkroasted bean'),
+    ('none','None'),
+)
+Milk = (
+    ('2% Milk','2%'),
+    ('non-fat','Non-fat'),
+    ('soy','Soy')
+    )
+Shot = (
+    ('double','Double'),
+    ('triple','Triple'),
+    ('quadruple','Quadruple'),
+    ('none','None')
+)
+class Drink(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.FloatField()
