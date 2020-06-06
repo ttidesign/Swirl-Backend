8acdb36c961887cb6084f7c2d8b200cb0cd3d77b
ttdesign <tkthedesigner@gmail.com>
2020-06-05 20:38:34 -0700

    install framework, add serialization, migrate data

---
+++ swirl_django/swirl/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Item, Order, OrderItem
+
+class ItemSerializer(serializers.HyperlinkedModelSerializer):
+
+   class Meta:
+       model = Item
+       fields=('id','title','description','ingredients', 'price','espresso','milk','shot','customize','img_url','preview_url',)
+
+class OrderItemSerializer(serializers.HyperlinkedModelSerializer):
+    item = serializers.HyperlinkedRelatedField(
+        view_name='item_detail',
+        read_only=True
+    )
+    class Meta:
+       model = OrderItem
+       fields=('id','item','quantity','ordered',)
+
+class OrderSerializer(serializers.HyperlinkedModelSerializer):
+    items = serializers.HyperlinkedRelatedField(
+        view_name='order_detail',
+        read_only=True
+    )
+    class Meta:
+       model = Order
+       fields=('id','items','start_date','ordered_date','ordered',)
--- swirl_django/swirl/urls.py
+++ swirl_django/swirl/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
-from .views import item_list, item_detail
+# from .views import item_list, item_detail
+from rest_framework.routers import DefaultRouter
 urlpatterns = [
-    path('', views.item_list, name='item_list'),
+    path('', views.ItemList.as_view(), name='item_list'),
+    path('cart/', views.OrderItem.as_view(), name='order_list'),
     path('drinks/<int:pk>', views.item_detail, name='item_detail'),
 ]
