1b0f90b05346de1b8dc309c12f97719ed3616c0c
ttdesign <tkthedesigner@gmail.com>
2020-06-11 09:55:26 -0700

    heroku deployment with template

--- swirl_django/swirl/views.py
+++ swirl_django/swirl/views.py
@@ -16,98 +16,98 @@ from .utils import cookieCart
 
 
 
-class CustomerList(generics.ListCreateAPIView):
-    queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer
+# class CustomerList(generics.ListCreateAPIView):
+#     queryset = Customer.objects.all()
+#     serializer_class = CustomerSerializer
 
-class UserList(generics.ListCreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# class UserList(generics.ListCreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
 
-class CustomerDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Customer.objects.all()
-    serializer_class = CustomerSerializer
+# class CustomerDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Customer.objects.all()
+#     serializer_class = CustomerSerializer
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
 
-class ItemList(generics.ListCreateAPIView):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
+# class ItemList(generics.ListCreateAPIView):
+#     queryset = Item.objects.all()
+#     serializer_class = ItemSerializer
 
-class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Item.objects.all()
-    serializer_class = ItemSerializer
+# class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Item.objects.all()
+#     serializer_class = ItemSerializer
 
-class OrderItem(generics.ListCreateAPIView):
-    queryset = OrderItem.objects.all()
-    serializer_class = OrderItemSerializer
+# class OrderItem(generics.ListCreateAPIView):
+#     queryset = OrderItem.objects.all()
+#     serializer_class = OrderItemSerializer
 
-class Order(generics.ListCreateAPIView):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
+# class Order(generics.ListCreateAPIView):
+#     queryset = Order.objects.all()
+#     serializer_class = OrderSerializer
         
 
-# def store(request):
-#     if request.user.is_authenticated:
-#         customer= request.user.customer
-#         order, created = Order.objects.get_or_create(customer=customer,ordered=False)
-#         items=order.orderitem_set.all()
-#         cartItems = order.get_cart_items
-#     else:
-#         cookieData = cookieCart(request)
-#         cartItems = cookieData['cartItems']
-#         # items =[]
-#         # order = {'get_cart_total':0,'get_cart_items':0}
-#         # cartItems = order['get_cart_items']
+def store(request):
+    if request.user.is_authenticated:
+        customer= request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,ordered=False)
+        items=order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+        # items =[]
+        # order = {'get_cart_total':0,'get_cart_items':0}
+        # cartItems = order['get_cart_items']
         
-#     items = Item.objects.all()
-#     context={'items':items, 'cartItems':cartItems}
-#     return render(request, 'swirl/store.html',context)
+    items = Item.objects.all()
+    context={'items':items, 'cartItems':cartItems}
+    return render(request, 'swirl/store.html',context)
 
-# def item_detail(request,pk):
-#     if request.user.is_authenticated:
-#         customer= request.user.customer
-#         order, created = Order.objects.get_or_create(customer=customer,ordered=False)
-#         items=order.orderitem_set.all()
-#         cartItems = order.get_cart_items
-#     else:
-#         cookieData = cookieCart(request)
-#         cartItems = cookieData['cartItems']
-#     item = Item.objects.get(id=pk)
-#     context={'item':item,'cartItems':cartItems}
-#     return render(request, 'swirl/item_detail.html',context)
+def item_detail(request,pk):
+    if request.user.is_authenticated:
+        customer= request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,ordered=False)
+        items=order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+    item = Item.objects.get(id=pk)
+    context={'item':item,'cartItems':cartItems}
+    return render(request, 'swirl/item_detail.html',context)
 
-# def checkout(request):
-#     if request.user.is_authenticated:
-#         customer= request.user.customer
-#         order, created = Order.objects.get_or_create(customer=customer,ordered=False)
-#         items=order.orderitem_set.all()
-#     else:
-#         cookieData = cookieCart(request)
-#         cartItems = cookieData['cartItems']
-#         order = cookieData['order']
-#         items = cookieData['items']
-#         # items =[]
-#         # order = {'get_cart_total':0,'get_cart_items':0}
-#     context={'items':items, 'order':order, 'cartItems':cartItems}
-#     return render(request, 'swirl/checkout.html',context)
+def checkout(request):
+    if request.user.is_authenticated:
+        customer= request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,ordered=False)
+        items=order.orderitem_set.all()
+    else:
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+        order = cookieData['order']
+        items = cookieData['items']
+        # items =[]
+        # order = {'get_cart_total':0,'get_cart_items':0}
+    context={'items':items, 'order':order, 'cartItems':cartItems}
+    return render(request, 'swirl/checkout.html',context)
 
-# def cart_detail(request):
-#     if request.user.is_authenticated:
-#         customer= request.user.customer
-#         order, created = Order.objects.get_or_create(customer=customer,ordered=False)
-#         items=order.orderitem_set.all()
-#         cartItems = order.get_cart_items
-#     else:
-#         cookieData = cookieCart(request)
-#         cartItems = cookieData['cartItems']
-#         order = cookieData['order']
-#         items = cookieData['items']
+def cart_detail(request):
+    if request.user.is_authenticated:
+        customer= request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,ordered=False)
+        items=order.orderitem_set.all()
+        cartItems = order.get_cart_items
+    else:
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+        order = cookieData['order']
+        items = cookieData['items']
 
-#     context={'items':items, 'order':order, 'cartItems':cartItems}
-#     return render(request, 'swirl/cart_detail.html',context)
+    context={'items':items, 'order':order, 'cartItems':cartItems}
+    return render(request, 'swirl/cart_detail.html',context)
 
 # @csrf_exempt
 def updateItem(request):
@@ -133,30 +133,30 @@ def updateItem(request):
         orderItem.delete()
     return JsonResponse('item was add',safe=False)
 
-# def processOrdder(request):
-#     transaction_id = datetime.datetime.now().timestamp()
-#     data = json.loads(request.body)
-#     if request.user.is_authenticated:
-#         customer = request.user.customer
-#         order, created = Order.objects.get_or_create(customer=customer,ordered=False)
-#         total = float(data['form']['total'])
-#         order.transaction_id = transaction_id
+def processOrdder(request):
+    transaction_id = datetime.datetime.now().timestamp()
+    data = json.loads(request.body)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer,ordered=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
 
-#         if total == float(order.get_cart_total):
-#             print(total)
-#             order.ordered = True
-#             order.save()
-#         ShippingAddress.objects.create(
-#             customer=customer,
-#             order=order,
-#             address=data['shipping']['address'],
-#             city=data['shipping']['city'],
-#             state=data['shipping']['state'],
-#             zipcode=data['shipping']['zipcode'],
-#         )
-#     else:
-#         print('User is not logged in')
-#     return JsonResponse('Payment complete!', safe=False)
+        if total == float(order.get_cart_total):
+            print(total)
+            order.ordered = True
+            order.save()
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+        )
+    else:
+        print('User is not logged in')
+    return JsonResponse('Payment complete!', safe=False)
 
 # @permission_classes((permissions.AllowAny,))
 # class AddToCartView(APIView):
